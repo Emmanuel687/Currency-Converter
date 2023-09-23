3058712a61e3250b709ae47bef1cb1da8db0671c
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-09-23 17:42:19 +0300

    Dev:-Set loading state in the application

--- src/App.js
+++ src/App.js
@@ -2,41 +2,66 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const[fromCur, setfromCur] =useState("EUR")
-  const[toCur, settoCur] =useState("USD")
-  const[amount, setAmount]=useState(1)
-  const[rate,setRate]=useState("")
+  const [fromCur, setfromCur] = useState("EUR");
+  const [toCur, settoCur] = useState("USD");
+  const [amount, setAmount] = useState(1);
+  const [rate, setRate] = useState("");
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    // const abortController = new AbortController();
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+          // abortController.signal
+        );
+        const data = await response.json();
+        setRate(data.rates[toCur]);
+      } catch (error) {
+        console.log("Error");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  useEffect(()=>{
-    const fetchData = async ()=>{
-      const response = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`)
-      const  data = await response.json()
-      setRate(data.rates)
-      console.log(rate)
-      
-    }
+    if (fromCur === toCur) return setRate(amount);
     fetchData()
-  },[amount])
 
-  // Amount in Target Currency = Amount in Source Currency * Exchange Rate
-    const exchange = amount*rate;
+    // return () => abortController.abort();
+  }, [amount, fromCur, toCur]);
 
   return (
     <div>
-      <input value={amount} type="text" onChange={(e)=>setAmount(Number(e.target.value))} />
-      <select value={fromCur} onChange={(e)=>setfromCur(e.target.value)}>
+      <input
+        value={amount}
+        type="text"
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={loading}
+      />
+      <select
+        value={fromCur}
+        onChange={(e) => setfromCur(e.target.value)}
+        disabled={loading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={toCur}  onChange={(e)=>settoCur(e.target.value)}>
+      <select
+        value={toCur}
+        onChange={(e) => settoCur(e.target.value)}
+        disabled={loading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT:-{}</p>
+      <p>
+        OUTPUT:-{rate} {toCur}
+      </p>
     </div>
   );
 }
