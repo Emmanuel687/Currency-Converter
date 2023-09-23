e23d8fc061ab68dca3134ea28d67cf276d884fd7
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-09-23 17:04:28 +0300

    Dev:-Renamed currency state

--- src/App.js
+++ src/App.js
@@ -2,33 +2,35 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const[currencyPair, setCurrencyPair] =useState("USD")
-  const[currencyPair2, setCurrencyPair2] =useState("EUR")
-  const[amount, setAmount]=useState(0)
-  const[rate, setRate] = useState("")
+  const[fromCur, setfromCur] =useState("EUR")
+  const[toCur, settoCur] =useState("USD")
+  const[amount, setAmount]=useState(1)
+  const[rate,setRate]=useState("")
 
   useEffect(()=>{
     const fetchData = async ()=>{
-      const response = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currencyPair}&to=${currencyPair2}`)
+      const response = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`)
       const  data = await response.json()
-      console.log(data.rates.currencyPair)
-      setRate(data.rates.currencyPair)
+      setRate(data.rates)
+      console.log(rate)
+      
     }
     fetchData()
   },[amount])
 
-
+  // Amount in Target Currency = Amount in Source Currency * Exchange Rate
+    const exchange = amount*rate;
 
   return (
     <div>
-      <input type="text" onChange={(e)=>setAmount(e.target.value)} />
-      <select onChange={(e)=>setCurrencyPair(e.target.value)}>
+      <input value={amount} type="text" onChange={(e)=>setAmount(Number(e.target.value))} />
+      <select value={fromCur} onChange={(e)=>setfromCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select  onChange={(e)=>setCurrencyPair2(e.target.value)}>
+      <select value={toCur}  onChange={(e)=>settoCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
